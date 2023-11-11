6c9af79949cb3eba2d27b735709ed9bd09c2409c
Viorel Breban <vbreban69@gmail.com>
2023-11-11 20:58:34 +0100

    last comit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,16 @@
-// script.js
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, "0")}`;
+    .padStart(6, '0')}`;
 }
 
-const startButton = document.querySelector("[data-start]");
-const stopButton = document.querySelector("[data-stop]");
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
 let intervalId = null;
 
-startButton.addEventListener("click", startColorChange);
-stopButton.addEventListener("click", stopColorChange);
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
 
 function startColorChange() {
   startButton.disabled = true;
