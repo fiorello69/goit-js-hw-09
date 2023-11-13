a2821f54a0bd7d6177f16fbc21101c96d3d8c629
Viorel Breban <vbreban69@gmail.com>
2023-11-13 12:32:32 +0100

    exercise number 1 with gradient

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,24 +4,32 @@ function getRandomHexColor() {
     .padStart(6, '0')}`;
 }
 
+function getConicGradient() {
+  const color1 = getRandomHexColor();
+  const color2 = getRandomHexColor();
+  const color3 = getRandomHexColor();
+
+  return `radial-gradient( circle,${color1}, ${color2}, ${color3})`;
+}
+
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
 let intervalId = null;
 
-startButton.addEventListener('click', startColorChange);
-stopButton.addEventListener('click', stopColorChange);
+startButton.addEventListener('click', startGradientChange);
+stopButton.addEventListener('click', stopGradientChange);
 
-function startColorChange() {
+function startGradientChange() {
   startButton.disabled = true;
   stopButton.disabled = false;
 
   intervalId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
+    document.body.style.background = getConicGradient();
   }, 1000);
 }
 
-function stopColorChange() {
+function stopGradientChange() {
   startButton.disabled = false;
   stopButton.disabled = true;
 
